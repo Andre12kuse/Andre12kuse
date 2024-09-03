0eb1bd8fbbc252af5bc4e205d27309b6abe5b3e0
Andre12kuse <148223222+Andre12kuse@users.noreply.github.com>
2024-09-03 21:06:51 +0100

    Create andrek.py
    just random python stuff enjoy :)

---
+++ andrek.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+def andre():
+    age = input("tell me your age :")
+    years = 2024-int(age)
+    print("you were born in",years,"!!!!!")
+
+for item in range (100):
+    print(item)
+
+window =  Tk()
+window.title("application")
+window.mainloop()
